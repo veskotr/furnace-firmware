6aafde81e33e28c64445ab88864b294dbf8f55de
vesko <vesko.trich@gmail.com>
2025-11-06 14:26:30 +0200

    Add log levels to logger component

--- components/logger-component/include/logger-component.h
+++ components/logger-component/include/logger-component.h
@@ -4,15 +4,25 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
 
+typedef enum
+{
+    LOG_LEVEL_ERROR,
+    LOG_LEVEL_WARN,
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_DEBUG,
+    LOG_LEVEL_VERBOSE
+} log_level_t;
+
 typedef struct
 {
     const char *tag;
     const char *message;
+    const log_level_t level;
 } log_message_t;
 
 
 void logger_init(void);
-void logger_send(const char *tag, const char *message);
+void logger_send(log_level_t log_level, const char *tag, const char *message);
 
 
 #endif
--- components/logger-component/logger-component.c
+++ components/logger-component/logger-component.c
@@ -10,9 +10,28 @@ static const char *LOG_TAG = "LOGGER";
 static void logger_task(void *pvParameter)
 {
     log_message_t msg;
-    while (1) {
-        if (xQueueReceive(logger_queue, &msg, portMAX_DELAY)) {
-            ESP_LOGI(msg.tag, "%s", msg.message);
+    while (1)
+    {
+        if (xQueueReceive(logger_queue, &msg, portMAX_DELAY))
+        {
+            switch (msg.level)
+            {
+            case LOG_LEVEL_WARN:
+                ESP_LOGW(msg.tag, "%s", msg.message);
+                break;
+            case LOG_LEVEL_ERROR:
+                ESP_LOGE(msg.tag, "%s", msg.message);
+                break;
+            case LOG_LEVEL_DEBUG:
+                ESP_LOGD(msg.tag, "%s", msg.message);
+                break;
+            case LOG_LEVEL_VERBOSE:
+                ESP_LOGV(msg.tag, "%s", msg.message);
+                break;
+            default:
+                ESP_LOGI(msg.tag, "%s", msg.message);
+                break;
+            }
         }
     }
 }
@@ -20,7 +39,8 @@ static void logger_task(void *pvParameter)
 void logger_init(void)
 {
     logger_queue = xQueueCreate(LOGGER_QUEUE_LEN, sizeof(log_message_t));
-    if (logger_queue == NULL) {
+    if (logger_queue == NULL)
+    {
         ESP_LOGE(LOG_TAG, "Failed to create queue");
         return;
     }
@@ -28,9 +48,8 @@ void logger_init(void)
     xTaskCreate(logger_task, "logger_task", 4096, NULL, 4, NULL);
 }
 
-void logger_send(const char *tag, const char *message)
+void logger_send(log_level_t log_level, const char *tag, const char *message)
 {
-    log_message_t msg = {.tag = tag, .message = message};
+    log_message_t msg = {.tag = tag, .message = message, .level = log_level};
     xQueueSend(logger_queue, &msg, 0); // Non-blocking
 }
-
