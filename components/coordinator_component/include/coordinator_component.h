2d6aa703ef63c11a88f75b9e840a0b579654a707
vesko <vesko.trich@gmail.com>
2025-11-18 14:56:45 +0200

    Small refactor

--- components/coordinator_component/include/coordinator_component.h
+++ components/coordinator_component/include/coordinator_component.h
@@ -2,15 +2,8 @@
 #define COORDINATOR_COMPONENT_H
 
 #include "esp_err.h"
-#include "core_types.h"
 
 esp_err_t init_coordinator();
-
-esp_err_t start_coordinator(heating_profile_t *profile);
-
-esp_err_t pause_coordinator(void);
-
-esp_err_t resume_coordinator(void);
 
 esp_err_t stop_coordinator(void);
 
--- components/coordinator_component/src/coordinator_component.c
+++ components/coordinator_component/src/coordinator_component.c
@@ -2,8 +2,16 @@
 
 #include "logger_component.h"
 
+#include "temperature_processor_component.h"
+#include "temperature_profile_controller.h"
+#include "temperature_monitor_component.h"
 
+esp_err_t init_coordinator() {
+    
+}
 
+esp_err_t stop_coordinator(void)
+{
+}
 
 #include "sdkconfig.h"
-
